079bef4d7d81cc7414e2442f0c124c0b7bdeef5f
Raquel Reyes <evelinreyeslem@gmail.com>
2025-03-13 22:49:01 -0600

    Estructura principal

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#Definir ruta padre
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+#Crear otra ruta
+@app.route("/personal")
+def personal():
+    return render_template("personal.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
